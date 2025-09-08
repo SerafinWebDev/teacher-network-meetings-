00bd9d3bc1b9ca744486d51ac2bbf9b7b853cbe5
SerafinWebDev <wom.katowice.tech@gmail.com>
2025-09-08 12:32:35 +0200

    add base connect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,72 @@
-import Image from "next/image";
+import { MySQLqueryToDatabase } from "@/services/connector";
 
-export default function Home() {
+export const revalidate = 3600; // revalidate the data at most every hour
+
+export default async function Home() {
+  const item = await MySQLqueryToDatabase(
+    `SELECT * FROM app_basic_training_units WHERE u_type = 'ssnid'; `
+  );
+  const dataObj = JSON.parse(item);
+  console.log(dataObj.rows);
+
+  const list = dataObj.rows;
+  const rowsToShow = [];
+
+  const now = Date.now();
+
+  list.forEach((meet) => {
+    const time2 = new Date(meet.to_at);
+
+    const maxShow = time2.getTime();
+    console.log(maxShow);
+    if (maxShow >= now) {
+      rowsToShow.push(meet);
+      console.log(meet.u_title);
+    }
+  });
+
+  const meetingToShow = rowsToShow.map((el) => {
+    return (
+      <div className="column2">
+        <div className="content">
+          <h2>{el.u_title}</h2>
+          <div className="flexbox">
+            <div className="col">
+              <h4>Data: {el.from_at} </h4>
+            </div>
+            <div className="col">
+              <h4>Data nadsyłania zgłoszeń: {el.join_to}</h4>
+            </div>
+          </div>
+          <div className="col">
+            {/* <h4>Prowadzący:</h4>
+            <div className="trainers-url">
+              <a
+                className="h4"
+                href="/o-osrodku/kadra,anna-czarlinska-wezyk-,61"
+              >
+                Anna Czarlińska-Wężyk{" "}
+              </a>
+            </div> */}
+          </div>
+        </div>
+        <div className="buttons-container">
+          {el.slug ?  <a className="button" href={`https://www.womkat.edu.pl/${el.slug}`} target="_top">
+            Więcej
+          </a> : '' }         
+        </div>
+      </div>
+    );
+  });
+  
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        {JSON.stringify(rowsToShow[0])}
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        {meetingToShow}
       </footer>
     </div>
   );
